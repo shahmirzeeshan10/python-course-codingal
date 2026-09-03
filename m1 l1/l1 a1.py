8a9ad4b8a253e1cbe4021c3c36c10f9e7bc445db
shahmirzeeshan10 <shahmirzeeshan10@gmail.com>
2026-09-03 09:31:39 -0400

    initial commit

---
+++ m1 l1/l1 a1.py
@@ -0,0 +1,42 @@
+print("my name is shahmir zeeshan")
+print("hello everyone")
+print(4)
+print("the number i printed",4)
+
+
+Chat
+
+
+Profile Image
+# My Snack Shop
+
+snack = "Chips"
+
+price = 1.50
+
+quantity = 10
+
+available = True
+
+# Data types
+
+print(snack, price, quantity, available)
+
+print(type(snack), type(price), type(quantity), type(available))
+
+# Arithmetic
+
+print("Total:", price * quantity)
+
+print("Sale price:", price - 0.25) #shop is giving 25% discount
+
+print("Double stock:", quantity * 2)
+
+# Comparison
+
+print("Cheap?", price < 2)
+
+print("Enough stock?", quantity > 5)
+
+print("Price is 1.50?", price == 1.50)
+
---
+++ m1 l2/l2 a1.py
@@ -0,0 +1,44 @@
+
+# My Snack Shop
+
+snack = "Chips"
+
+price = 1.50
+
+quantity = 10
+
+available = True
+
+# Data types
+
+print(snack, price, quantity, available)
+
+print(type(snack), type(price), type(quantity), type(available))
+
+# Arithmetic
+
+print("Total:", price * quantity)
+
+print("Sale price:", price - 0.25) #shop is giving 25% discount
+
+print("Double stock:", quantity * 2)
+
+# Comparison
+
+print("Cheap?", price < 2)
+
+print("Enough stock?", quantity > 5)
+
+print("Price is 1.50?", price == 1.50)
+shop="Quick" +" bites"
+print(shop)
+print(len(snack))
+print(snack[0])
+price_a=1.50
+price_b=3.00
+temp =price_a
+price_a=price_b
+price_b= temp
+print("after swap :", price_a, "and" , price_b)
+
+
