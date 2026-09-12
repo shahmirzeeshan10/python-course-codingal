d6f2c3e823abf6c1065cb3c833aeba040febc71a
shahmirzeeshan10 <shahmirzeeshan10@gmail.com>
2026-09-12 05:00:49 -0400

    initial commit

---
+++ ml l5/calculator.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+
+number1=int(input("enter your first number"))
+number2 = int(input("enter your second number"))
+print("the sum of 2 numbers is ",add(number1,number2))
+print("the subtraction of 2 numbers is ",subtract(number1,number2))
+print("the product of 2 numbers is ",multiply(number1,number2))
+print("the division of 2 numbers is ",divide(number1,number2))
+
+
---
+++ ml l5/intro.py
@@ -0,0 +1,4 @@
+def intro(name):
+    print("hello everyone i am ", name)
+user_name= input("enter your name")
+intro(user_name)
